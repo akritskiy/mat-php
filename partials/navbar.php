781436d4fc6936de023100c8bdc116e6a98e95fa
akritskiy <akritskiy@gmail.com>
2019-04-22 23:39:26 -0400

    moved collapse toggle btn to left, added some conditional rendering to homepage get started btn... a logged in user sees get started w/ the URL going to dash, a non-logged in user sees two btns, login and register

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,8 +1,8 @@
 <nav id="navbar" class="navbar navbar-expand-sm navbar-dark">
-  <a id="brand" class="navbar-brand" href="index.php">Matchisuru</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+  <a id="brand" class="navbar-brand" href="index.php">Matchisuru</a>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav ml-auto">
       <?php
